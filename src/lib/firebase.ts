a347987cb49b06f80ffb58a43404c86d9372562c
Wasim Rahman <rahmanwasim67@gmail.com>
2025-08-07 03:55:35 +0000

    bro i enabled the email/password option. But still it didn't worked, it

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// IMPORTANT: Replace with your own Firebase project configuration.
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
